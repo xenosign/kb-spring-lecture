7fbb7e7727c622c19f371b388e835258d3296710
kdtTetz <xenosign@naver.com>
2024-08-16 20:24:06 +0900

    Update JpaConfig.java

--- src/main/java/org/example/config/JpaConfig.java
+++ src/main/java/org/example/config/JpaConfig.java
@@ -1,11 +1,9 @@
 package org.example.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -55,4 +53,5 @@ public class JpaConfig {
         return properties;
     }
 }
+
 
