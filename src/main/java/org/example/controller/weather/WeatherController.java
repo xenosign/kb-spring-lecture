8c7104a507327f3541d57684dd59184bea79428e
Tetz <xenosign@naver.com>
2024-08-22 17:09:50 +0900

    Update WeatherController.java

--- src/main/java/org/example/controller/weather/WeatherController.java
+++ src/main/java/org/example/controller/weather/WeatherController.java
@@ -18,7 +18,6 @@ public class WeatherController {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String URI = "https://api.openweathermap.org/data/2.5/weather";
     private final String UNITS = "metric";
-    private final String API_KEY = "590c223343799397b190b4fa8894ebf2";
     private final String LANG = "kr";
 
     @GetMapping("/test/{city}")
