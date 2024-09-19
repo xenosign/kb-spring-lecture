553df9705fdf40d0c911a1cdbda8aabe317b9be2
Tetz <xenosign@naver.com>
2024-09-19 17:47:52 +0900

    Update AuthenticationInterceptor.java

--- src/main/java/org/example/security/AuthenticationInterceptor.java
+++ src/main/java/org/example/security/AuthenticationInterceptor.java
@@ -9,18 +9,18 @@ import javax.servlet.http.HttpSession;
 
 @Component
 public class AuthenticationInterceptor implements HandlerInterceptor {
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//            throws Exception {
-//        String requestUri = request.getRequestURI();
-//        System.out.println("요청 URI: " + requestUri);
-//
-//        HttpSession session = request.getSession();
-//        if (session.getAttribute("loginUser") == null) {
-//            System.out.println("로그인 안됨. 리다이렉트 중: /user/login");
-//            response.sendRedirect("/user/login");
-//            return false;
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String requestUri = request.getRequestURI();
+        System.out.println("요청 URI: " + requestUri);
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loginUser") == null) {
+            System.out.println("로그인 안됨. 리다이렉트 중: /user/login");
+            response.sendRedirect("/user/login");
+            return false;
+        }
+        return true;
+    }
 }
